259be44dd0642c6024de4b863c12ea47281e02d9
MVA <m.vantorre@televic.com>
2025-01-23 09:38:10 +0100

    Add gitblame

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -94,6 +94,9 @@ require('lazy').setup({
   -- VimTex
   require 'custom/plugins/vimtex',
 
+  -- Git-blame
+  require 'custom.plugins.git-blame',
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
