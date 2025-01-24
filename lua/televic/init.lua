2c1b342a716f4f91d1809818f0dce743564a76b7
MVA <m.vantorre@televic.com>
2025-01-24 16:06:03 +0100

    Update toolchain location

--- lua/televic/init.lua
+++ lua/televic/init.lua
@@ -1,5 +1,5 @@
 return {
   plixus_apps_build_dir = 'build_pc',
-  build_scripts_path = vim.fn.expand '$HOME/code/build_scripts/',
-  toolchainfile = 'televic_pc_sdk_2023.02.7',
+  build_scripts_path = vim.fn.expand '$HOME/.local/share/build_scripts/toolchains/',
+  toolchainfile = 'televic_pc_sdk_2023.02.12',
 }
