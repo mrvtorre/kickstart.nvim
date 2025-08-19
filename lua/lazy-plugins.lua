4bea4f898f1c637f2315c85b299763da39df8793
MVA <m.vantorre@televic.com>
2025-08-19 10:45:07 +0200

    Add startup screen

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -97,6 +97,9 @@ require('lazy').setup({
   -- Git-blame
   require 'custom.plugins.git-blame',
 
+  -- Alpha: startup screen
+  require 'custom.plugins.alpha',
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
