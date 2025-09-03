bb2c9baa5707e9910c5cb8605bf6caa05f097dfa
MVA <m.vantorre@televic.com>
2025-09-03 11:37:35 +0200

    Update toolchain path

--- lua/televic/init.lua
+++ lua/televic/init.lua
@@ -1,5 +1,5 @@
 return {
   plixus_apps_build_dir = 'build_pc',
   build_scripts_path = vim.fn.expand '$HOME/.local/share/build_scripts/toolchains/',
-  toolchainfile = 'televic_pc_sdk_2023.02.12',
+  toolchainfile = 'televic_pc_sdk_2025.02.10'
 }
