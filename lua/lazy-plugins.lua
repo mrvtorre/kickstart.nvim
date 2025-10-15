df9fd42ac4f2a9908a7122a8f3576ea0963ef578
MVA <m.vantorre@televic.com>
2025-10-15 14:26:08 +0200

    Format

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -39,7 +39,7 @@ require('lazy').setup({
       },
     },
   },
-  { 'Bilal2453/luvit-meta',     lazy = true },
+  { 'Bilal2453/luvit-meta', lazy = true },
   {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
@@ -52,7 +52,7 @@ require('lazy').setup({
 
       -- Useful status updates for LSP.
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim',       opts = {} },
+      { 'j-hui/fidget.nvim', opts = {} },
 
       -- Allows extra capabilities provided by nvim-cmp
       'hrsh7th/cmp-nvim-lsp',
@@ -111,8 +111,8 @@ require('lazy').setup({
 
   -- vim-kitty: syntax highlighting for Kitty terminal configuration and session files
   {
-    "fladson/vim-kitty",
-    ft = "kitty",
+    'fladson/vim-kitty',
+    ft = 'kitty',
   },
 
   -- Comment.nvim: Smart and Powerful commenting plugin for neovim
