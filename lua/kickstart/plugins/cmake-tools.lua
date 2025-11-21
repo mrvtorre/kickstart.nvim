3c223ca6962a2841ce61fd080847e10347a8ef47
MVA <m.vantorre@televic.com>
2025-11-21 08:48:07 +0100

    Make sure a default kit is selected in order to avoid a popup

--- lua/kickstart/plugins/cmake-tools.lua
+++ lua/kickstart/plugins/cmake-tools.lua
@@ -1,9 +1,24 @@
 return {
-    'Civitasv/cmake-tools.nvim',
-    config = function()
-        require('cmake-tools').setup {
-            cmake_build_directory = require('televic').plixus_apps_build_dir,
-            cmake_kits_path = vim.fn.expand '$HOME/.local/share/CMakeTools/cmake-tools-kits.json',
-        }
-    end,
+  'Civitasv/cmake-tools.nvim',
+  config = function()
+    local cmake = require 'cmake-tools'
+    cmake.setup {
+      cmake_build_directory = require('televic').plixus_apps_build_dir,
+      cmake_kits_path = vim.fn.expand '$HOME/.local/share/CMakeTools/cmake-tools-kits.json',
+      cmake_regenerate_on_save = true,
+    }
+
+    -- Auto-select default kit on first FileType detection for CMake files
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'cmake',
+      once = true,
+      callback = function()
+        local config = require 'cmake-tools.config'
+        if not config.kit then
+          -- Silently set the kit without showing the selection UI
+          config.kit = 'Televic PC SDK'
+        end
+      end,
+    })
+  end,
 }
